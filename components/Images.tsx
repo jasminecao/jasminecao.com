73e44ad0c891c5f1ab6c5568f5b042c6ccd148d9
Jasmine Cao <jcao3@seas.upenn.edu>
2021-12-21 23:58:05 -0500

    fixes

---
+++ components/Images.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Images = () => {
+  const images = [
+    'llama',
+    'chinatown',
+    'girl-with-ducks',
+    'central-park',
+    'badminton-action-shot',
+    'lighthouse',
+    'lui-claud',
+    'nyc',
+    'philly',
+    'mt-rainier',
+    'seattle',
+    'turtle-frog',
+  ]
+
+  return images.map((img) => (
+    <div key={img} style={{ margin: '0.5rem', display: 'inline-block' }}>
+      <Image
+        key={img}
+        src={`/film/${img}.jpg`}
+        width="450"
+        height="300"
+        alt={img}
+      />
+    </div>
+  ))
+}
+
+export default Images
