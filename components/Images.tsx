e1ca215eab31ab5ee53f251b3facff49da2daf78
Jasmine Cao <jcao3@seas.upenn.edu>
2022-01-03 00:08:59 -0500

    fix image layout

--- components/Images.tsx
+++ components/Images.tsx
@@ -1,33 +1,53 @@
 import Image from 'next/image'
 import React from 'react'
+import llama from '@/public/film/llama.jpg'
+import chinatown from '@/public/film/chinatown.jpg'
+import girlWithDucks from '@/public/film/girlWithDucks.jpg'
+import centralPark from '@/public/film/centralPark.jpg'
+import badmintonActionShot from '@/public/film/badmintonActionShot.jpg'
+import lighthouse from '@/public/film/lighthouse.jpg'
+import luiClaud from '@/public/film/luiClaud.jpg'
+import nyc from '@/public/film/nyc.jpg'
+import philly from '@/public/film/philly.jpg'
+import mtRainier from '@/public/film/mtRainier.jpg'
+import seattle from '@/public/film/seattle.jpg'
+import turtleFrog from '@/public/film/turtleFrog.jpg'
 
 const Images = () => {
   const images = [
-    'llama',
-    'chinatown',
-    'girl-with-ducks',
-    'central-park',
-    'badminton-action-shot',
-    'lighthouse',
-    'lui-claud',
-    'nyc',
-    'philly',
-    'mt-rainier',
-    'seattle',
-    'turtle-frog',
+    llama,
+    chinatown,
+    girlWithDucks,
+    centralPark,
+    badmintonActionShot,
+    lighthouse,
+    luiClaud,
+    nyc,
+    philly,
+    mtRainier,
+    seattle,
+    turtleFrog,
   ]
 
-  return images.map((img) => (
-    <div key={img} style={{ margin: '0.5rem', display: 'inline-block' }}>
-      <Image
-        key={img}
-        src={`/film/${img}.JPG`}
-        width="450"
-        height="300"
-        alt={img}
-      />
+  return (
+    <div className="image-container">
+      {images.map((img, i) => {
+        const imageName = img.src.split('/').slice(-1)[0].split('.')[0]
+        return (
+          <div className="image-wrapper" key={`${imageName}-wrap`}>
+            <Image
+              key={`${imageName}-img`}
+              src={img}
+              alt={imageName}
+              placeholder="blur"
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+        )
+      })}
     </div>
-  ))
+  )
 }
 
 export default Images
